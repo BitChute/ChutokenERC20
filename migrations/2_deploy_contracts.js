5c2b02d8b5ae95630e9aefb7def4623d9c4aaefd
Ray <ray@bitchute.com>
2019-09-13 14:24:35 +0700

    token sale

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
 const Chutoken = artifacts.require("Chutoken");
+const ChutokenSale = artifacts.require("ChutokenSale");
 
 module.exports = function(deployer) {
-  deployer.deploy(Chutoken, 3141592653);
+  deployer.deploy(Chutoken, 3141592653).then(function() {
+    // Token price is 0.001 Ether
+    var tokenPrice = 1000000000000000;
+    return deployer.deploy(ChutokenSale, Chutoken.address, tokenPrice);
+  });
 };
