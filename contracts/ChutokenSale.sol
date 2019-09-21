eb9b366be43f595cbb2410facb1510dfa8ecbead
Ray <ray@bitchute.com>
2019-09-21 23:50:30 +0700

    added setTokenPrice function

--- contracts/ChutokenSale.sol
+++ contracts/ChutokenSale.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.8;
+pragma solidity >= 0.5.0 < 0.6.0;
 
 import "./Chutoken.sol";
 
@@ -7,6 +7,7 @@ contract ChutokenSale {
     Chutoken public tokenContract;
     uint256 public tokenPrice;
     uint256 public tokensSold;
+    uint256 public dollarValue;
 
     event Sell(address _buyer, uint256 _amount);
 
@@ -21,17 +22,22 @@ contract ChutokenSale {
     }
 
     function buyTokens(uint256 _numberOfTokens) public payable {
-        require(msg.value == multiply(_numberOfTokens, tokenPrice));
-        require(tokenContract.balanceOf(address(this)) >= _numberOfTokens);
-        require(tokenContract.transfer(msg.sender, _numberOfTokens));
-        tokensSold += _numberOfTokens;
-        emit Sell(msg.sender, _numberOfTokens);
+    	require(msg.value == multiply(_numberOfTokens, tokenPrice));
+    	require(tokenContract.balanceOf(address(this)) >= _numberOfTokens);
+    	require(tokenContract.transfer(msg.sender, _numberOfTokens));
+    	tokensSold += _numberOfTokens;
+    	emit Sell(msg.sender, _numberOfTokens);
     }
 
-    function endSale() public {
-        require(msg.sender == admin, "only admin can end the sale");
-    	require(tokenContract.transfer(admin, tokenContract.balanceOf(address(this))),"failed to transfer balance");
-        admin.transfer(address(this).balance);
-        selfdestruct(admin);
-    }
+    function setTokenPrice(uint256 _value) public {
+    	require(msg.sender == admin);
+    	tokenPrice = _value;
+    } 
+
+    //function endSale() public {
+    //    require(msg.sender == admin, "only admin can end the sale");
+    //	  require(tokenContract.transfer(admin, tokenContract.balanceOf(address(this))),"failed to transfer balance");
+    //    admin.transfer(address(this).balance);
+    //    selfdestruct(admin);
+    //}
 }
